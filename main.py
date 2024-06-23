155dcab915d23b4f7302667602975a1fbec1b0b4
Adrian <adriankon89@gmail.com>
2024-06-23 11:30:02 +0200

    fix: Handle cards when is tie

--- main.py
+++ main.py
@@ -10,22 +10,18 @@ class Game:
         self.players = players
 
     def play(self):
-        turn_amount = 0
+        turn_counter = 0
         while len(self.players) != 1:
-            print("start")
-
             self.draw_card_by_players()
-
             user_who_won = self.battle()
             if user_who_won:
                 self.add_cards_to_winner_of_battle(user_who_won)
 
             self.remove_player_without_cards()
             self.__card_pool.clear()
-            turn_amount += 1
-
+            turn_counter += 1
         winner = self.players.pop()
-        print(f"won {winner} {len(winner.cards)} with turn of {turn_amount}")
+        print(f"won {winner} {len(winner.cards)} with turn of {turn_counter}")
 
     def remove_player_without_cards(self):
         self.players = [player for player in self.players if player.has_cards()]
@@ -37,23 +33,15 @@ class Game:
                 self.__card_pool.append((player, card))
 
     def battle(self):
-        # todo TIE/draw if two pair of users has a draw
         for player, card in self.__card_pool:
             print(f"Player {player} card {card.rank}")
-
         highest_cards = self.get_highest_pool_cards(self.__card_pool)
-
         if len(highest_cards) > 1:
             player_who_win_battle = self.resolve_tie(highest_cards)
-
         else:
             player_who_win_battle, _ = highest_cards[0]
 
         return player_who_win_battle
-
-    def add_cards_to_winner_of_battle(self, winner_player):
-        for _, card in self.__card_pool:
-            winner_player.add_card(card)
 
     def get_highest_pool_cards(self, card_pool):
         highest_strength = max(card.strength for _, card in card_pool)
@@ -70,10 +58,11 @@ class Game:
         """check if users has a card if not remove them, draw a card get a highest card
         """
         for player, card in highest_cards:
+            print(f"TIE: Player {player} with card {card.rank}")
             self.__stash_card_pool.append(card)
             if player.has_cards():
                 tied_players.append(player)
-            print(f"Player {player} with card {card.rank}")
+
         self.__card_pool.clear()
 
         tied_players_amount = len(tied_players)
@@ -86,6 +75,7 @@ class Game:
 
     def tie_battle(self, players):
         for player in players:
+            print("player len cards ", len(player.cards))
             card = player.draw_card()
             if card:
                 self.__card_pool.append((player, card))
@@ -94,16 +84,22 @@ class Game:
         if len(highest_cards) > 1:
             self.resolve_tie(highest_cards)
             return False
-
         player_who_win_battle, _ = highest_cards[0]
 
         return player_who_win_battle
 
+    def add_cards_to_winner_of_battle(self, winner_player):
+        for _, card in self.__card_pool:
+            winner_player.add_card(card)
 
-# Example usage
+        for card in self.__stash_card_pool:
+            winner_player.add_card(card)
+
+        self.__card_pool.clear()
+        self.__stash_card_pool.clear()
+
+
 num_players = 4
-
-# deck = Deck(num_players)
 
 
 def create_players(num_players):
